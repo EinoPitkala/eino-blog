01bd5372da7c915923207ae0ee284d697fe37e45
Eino Pitkälä <eiccabot@gmail.com>
2023-05-03 18:41:42 +0300

    Made navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+        <div className="nav-logo">
+            <h1>Logo</h1>
+        </div>
+        <div className="nav-links">
+            <a href="/">Nav link1</a>
+            <a href="/">Nav link 2</a>
+        </div>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
